73f8ad1640afed834c240e86526b19dba92b3d90
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-08 00:03:57 +0700

    menampilkan anggota paling pertama dari list

--- coba-perulangan-for.py
+++ coba-perulangan-for.py
@@ -1,12 +1,13 @@
-angka=[1,2,3,4,5]
+angka = [1, 2, 3, 4, 5]
 for x in angka:
+    print("urutan paling depan/index pertama=", angka[0])
     print(x)
-    
 
-buah=["apel","jeruk","semangka"]
+
+buah = ["apel", "jeruk", "semangka"]
 for makanan in buah:
-    print("saya suka makan",makanan)
-    
+    print("saya suka makan", makanan)
+
 
 # i=2
 # j=2
