7d79879ca461615bf417e4ae50e4716cd85db134
naufalHaidar12342 <1111912342@mhs.dinus.ac.id>
2021-03-18 20:43:31 +0700

    menambahkan penjelasan/comment

--- iseng-iseng/praktek2.py
+++ iseng-iseng/praktek2.py
@@ -1,9 +1,19 @@
+# langkah pertama, kita minta input dari user
+# huruf n bisa diganti nama lain
 n = int(input("masukkan angka sembarang ="))
+
+# seperti biasa, print("") untuk menghasilkan baris kosong
 print("")
 
+# supaya bisa dihitung digitnya
+# angka tersebut kita ubah ke string dulu
 ubahKeStr = str(n)
+
+# fungsi len [length] menghitung panjang string
 panjangAngka = len(ubahKeStr)
 
+# yang akan dipanggil adalah "n" dan
+#  "panjangAngka"
 if len(ubahKeStr) == 1:
     print("angka {} terdiri dari {} digit".format(n, panjangAngka))
 
